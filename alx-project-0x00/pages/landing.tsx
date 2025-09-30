8731c100773c8198172ffa47eac3897c84770953
sewalewsetotaw <sewalewsetotaw2023@gmail.com>
2025-09-30 23:22:31 +0300

    Using components

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,19 @@
-const Landing: React.FC =  () => {
+import React from "react"
+import Card from "@/components/Card"
+
+const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
-    </div>
+    <main className="flex flex-col h-screen justify-center items-center text-2xl font-semibold gap-6">
+      <h1>Available Properties</h1>
+
+      {/* Render one card */}
+      <Card />
+
+      {/* Duplicate to test reusability */}
+      <Card />
+      <Card />
+    </main>
   )
 }
+
 export default Landing

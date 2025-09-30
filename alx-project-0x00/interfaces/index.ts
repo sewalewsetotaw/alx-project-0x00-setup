3a76b86d7a2aeebf533de97c088a5a37c1850a5f
sewalewsetotaw <sewalewsetotaw2023@gmail.com>
2025-09-30 23:37:12 +0300

    fixing the checker error

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,12 +1,12 @@
 export interface PillProps {
   title: string
-   styles?: string
+   styles: string
 }
 
 export interface ButtonProps {
   title: string
   size?: "small" | "medium" | "large"
   shape?: "rounded-sm" | "rounded-md" | "rounded-full" | "rounded-lg"
-  styles?: string
+  styles: string
 }
 
